868438e324d58da33375170718126b354a47b42c
Levi Broderick <levib@yahoo.com>
2012-06-05 08:42:02 -0700

    Initial commit of project files.

---
+++ Web/Util/NoopRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web;
+using System.Web.Util;
+
+namespace Pitchfork.Web.Util {
+    /// <summary>
+    /// A RequestValidator that performs no work.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public sealed class NoopRequestValidator : RequestValidator {
+        protected override bool IsValidRequestString(HttpContext context, string value, RequestValidationSource requestValidationSource, string collectionKey, out int validationFailureIndex) {
+            validationFailureIndex = 0;
+            return true;
+        }
+    }
+}
